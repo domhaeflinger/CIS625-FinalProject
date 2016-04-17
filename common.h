e9fca66a557e2cf6258c59553108e5bf39d29be9
domhaeflinger <domhaeflinger@gmail.com>
2016-04-17 12:46:16 -0500

    Added end if at the end of common.h

--- common.h
+++ common.h
@@ -27,3 +27,5 @@ double read_timer();
 int find_option( int argc, char **argv, const char *option );
 int read_int( int argc, char **argv, const char *option, int default_value );
 char *read_string( int argc, char **argv, const char *option, char *default_value );
+
+#endif
