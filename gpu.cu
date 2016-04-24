11f6dbe4ae556cf685d78d9bd8f8b1e356b4a614
domhaeflinger <domhaeflinger@gmail.com>
2016-04-24 14:30:36 -0500

    more changes to unsigned ints

--- gpu.cu
+++ gpu.cu
@@ -58,7 +58,7 @@ __device__ void calcArrayIndex(int *index, int adjN, int adjY, int x){
 }
 
 // Calculate the position in the matrix
-__device__ void calcPosInMatrix(int index, int n, int *x, int *y){
+__device__ void calcPosInMatrix(int index, int n, unsigned short *x, unsigned short *y){
   calcXPos(x, index * 2, n - (.5f));
   calcYPos(y, index + 1, 3 - 2 * n, *x);
 }
